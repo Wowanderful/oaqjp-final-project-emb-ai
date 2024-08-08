f43772f3a4b1abe4e27939c4c147f5744c10736a
Wowanderful <wowanderful@gmail.com>
2024-08-08 18:02:11 -0400

    8 task complete

--- server.py
+++ server.py
@@ -1,25 +1,35 @@
+# Importing Flask
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask("Emotion Detector")
 
+#Approute
 @app.route("/emotionDetector")
+
+# Main function
 def em_detector():
+
+    # Get the text to analyze from the query parameters
     text_to_analyze = request.args.get('textToAnalyze')
 
+    # Call the emotion detection function
     response = emotion_detector(text_to_analyze)
 
-    list = response[0]
+    # Extract the list of emotions and the dominant emotion score
+    li = response[0]
     dom = response[2]
 
-    if dom == None:
+    # Check if a valid emotion was detected
+    if dom is None:
         return "Invalid text! Please try again!"
-    else: 
-        return "The given text has been identified as {} with a score of {}.".format(list, dom)
+    # Return the detected emotion and score
+    return f"The given text has been identified as {li} with a score of {dom}."
 
+# approute
 @app.route("/")
 def render_index_page():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001)
+    app.run(host="0.0.0.0", port=5002)
