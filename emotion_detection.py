b9f0861acb6277505763579b5369d23587800b26
Wowanderful <wowanderful@gmail.com>
2024-08-08 14:00:34 -0400

    3 task in progress

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,8 +1,13 @@
-import requests  # Import the requests library to handle HTTP requests
+import requests
+import json  
 
 def emotion_detector(text_to_analyse):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'  
     myobj = { "raw_document": { "text": text_to_analyse } }  
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"} 
-    response = requests.post(url, json = myobj, headers=header)  
-    return response.text 
+    response = requests.post(url, json = myobj, headers=header) 
+    formatted_response = json.loads(response.text) 
+    # label = formatted_response['emotionPredictions']['emotion']
+    # score = formatted_response['documentSentiment']['score']
+    return formatted_response['emotionPredictions'][0]
+
